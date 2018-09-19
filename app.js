1af50067cb2e16f9ec1b35513d6e75870b6e0c83
belqati <nahnahgames@gmail.com>
2018-09-19 13:34:26 -0500

    add fetch user data from github

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
+// Instantiate github class from github.js
+const github = new Github;
+
 // Search input
 const searchUser = document.querySelector('#searchUser');
 
 // Search input event listener
 searchUser.addEventListener('keyup', (e) => {
-  // Get input text
+  // Get input text from event object
   const userText = e.target.value;
 
   if(userText !== ''){
-    console.log(userText);
+    // make http call
+    github.getUser(userText)
+      .then(data => {
+        // profile from github.js
+        if(data.profile.message === 'Not Found'){
+          // show alert
+        } else {
+          // show profile
+        }
+    });
+  } else {
+    // clear profile
   }
 });
--- public/scripts/github.js
+++ public/scripts/github.js
@@ -1,6 +1,18 @@
-class GitHub {
+class Github {
   constructor(){
     this.client_id = process.env.GIT_CLIENT_ID;
     this.client_secret = process.env.GIT_CLIENT_SECRET;
+  }
+
+  // get user and user repos
+  async getUser(user){
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+
+    return {
+      // in ES6 profile: profile unecessary
+      profile
+    }
   }
 }
